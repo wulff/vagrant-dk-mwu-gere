9ba472150fb8ae68114e89734e53c52ce1b142cc
Morten Wulff <wulff@ratatosk.net>
2014-02-05 11:24:40 +0100

    Removed trailing whitespace

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -53,11 +53,11 @@ class gere::install {
 
   # configure postfix as a null client
   # http://www.postfix.org/STANDARD_CONFIGURATION_README.html#null_client
-  
+
   class { 'postfix':
     hostname => 'gere.mwu.dk',
   }
-  
+
   # install the irssi client
 
   class { 'irssi':
