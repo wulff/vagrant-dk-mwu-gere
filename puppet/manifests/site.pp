68d437e95febf0bb63506ea2845e8f56619df67d
wulff <wulff@ratatosk.net>
2014-06-23 20:39:23 +0200

    Removed bash module

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -148,7 +148,12 @@ class gere::install {
     name => 'Europe/Copenhagen',
   }
 
-  class { 'bash': }
+  file { '/etc/profile.d/aliases.sh':
+    content => 'alias update="sudo apt-get update"
+alias upgrade="sudo apt-get upgrade"
+alias puppet-apply="sudo puppet apply --modulepath=/home/wulff/vagrant/puppet/modules/ /home/wulff/vagrant/puppet/manifests/site.pp"',
+    mode => 0644,
+  }
 }
 
 class gere::go {
--- puppet/modules/bash/files/aliases.sh
+++
@@ -1,3 +0,0 @@
-alias update="sudo apt-get update"
-alias upgrade="sudo apt-get upgrade"
-alias puppet-apply="sudo puppet apply --modulepath=/home/wulff/vagrant/puppet/modules/ /home/wulff/vagrant/puppet/manifests/site.pp"
